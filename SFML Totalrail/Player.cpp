687f188639481cb8ea252de63c5a9ba25f8fdca7
PLEUM Supakiart <oyashi11110@gmail.com>
2020-10-27 12:28:11 +0700

    Add project files.

---
+++ SFML Totalrail/Player.cpp
@@ -0,0 +1,88 @@
+#include "Player.h"
+
+Player::Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed,float jumpHeight) :
+	animation(texture, imageCount, switchTime)
+{
+	this->speed = speed;
+	this->jumpHeight = jumpHeight;
+	row = 0;
+	faceRight = true;
+
+
+	body.setSize(sf::Vector2f(60.0f, 80.0f));
+	body.setOrigin(body.getSize() / 2.0f);
+	body.setPosition(540.0, 360.0f);
+	body.setTexture(texture);
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::Update(float deltaTime)
+{
+	velocity.x = 0.0f;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		velocity.x -= speed*5;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		velocity.x += speed*5;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && canJump)
+	{
+		canJump = false;
+		velocity.y = -sqrtf(2.0f * 981.0f * jumpHeight*5);
+		// squar root ( 2.0f * gravity * jumpHeight);
+	}
+
+	velocity.y += 4 * 981.0f * deltaTime;
+	
+	if (velocity.x == 0.0f)
+	{
+		row = 0;
+	}
+	else
+	{
+		row = 1;
+
+		if (velocity.x > 0.0f)
+			faceRight = true;
+		else
+			faceRight = false;
+	}
+
+	animation.Update(row, deltaTime, faceRight);
+	body.setTextureRect(animation.uvRect);
+	body.move(velocity * deltaTime);
+}
+
+void Player::Draw(sf::RenderWindow& window)
+{
+	window.draw(body);
+}
+
+void Player::OnCollision(sf::Vector2f direction)
+{
+	if (direction.x < 0.0f)
+	{
+		// Collision on the left.
+		velocity.x = 0.0f;
+	}
+	else if (direction.x > 0.0f)
+	{
+		// Collision on the right.
+		velocity.x = 0.0f;
+	}
+	if (direction.y < 0.0f)
+	{
+		// Collision on the bottom.
+		velocity.y = 0.0f;
+		canJump = true;
+	}
+	else if (direction.y > 0.0f)
+	{
+		// Collision on the top.
+		velocity.y = 0.0f;
+	}
+}
---
+++ SFML Totalrail/SFML Totalrial.cpp
@@ -0,0 +1,76 @@
+#include<SFML/Graphics.hpp>
+#include<vector>
+#include<iostream>
+#include "Player.h"
+#include "Platform.h"
+
+static const float VIEW_HEIGHT = 512.0f;
+
+void ResizeView(const sf::RenderWindow& window, sf::View& view)
+{
+    float aspecRatio = float(window.getSize().x) / float(window.getSize().y);
+    view.setSize(VIEW_HEIGHT * aspecRatio, VIEW_HEIGHT);
+}
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "GUN SLUG", sf::Style::Close | sf::Style::Resize);
+    sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(VIEW_HEIGHT, VIEW_HEIGHT));
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("run.png");
+
+    Player player(&playerTexture, sf::Vector2u(6, 2), 0.11f, 100.0f, 400.0f); // rate of picture sprite a little dramatically >> so fast (speed in x, jump high in y)
+
+    std::vector<Platform> platforms;
+ 
+    platforms.push_back(Platform(nullptr, sf::Vector2f(400.0f, 200.0f), sf::Vector2f(500.0f, 0.0f)));      // (wild,high)  (left,right)
+    platforms.push_back(Platform(nullptr, sf::Vector2f(400.0f, 200.0f), sf::Vector2f(700.0f, 200.0f)));       // (wild,high)  ((left,right),(up,down)) point+ box right/down ,point- box left/up 
+    platforms.push_back(Platform(nullptr, sf::Vector2f(1000.0f, 100.0f), sf::Vector2f(500.0f, 500.0f))); //floor   // (wild,high)  (left,right)
+    
+    float deltaTime = 0.0f;
+    sf::Clock clock;
+
+    while (window.isOpen())
+    {
+        deltaTime = clock.restart().asSeconds();
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::Resized:
+                ResizeView(window, view);
+                break;
+            }
+        }
+
+        player.Update(deltaTime);
+
+        sf::Vector2f direction;
+
+        for (Platform& platform : platforms)
+            if (platform.GetCollider().CheckCollision(&player.GetCollider(), direction, 1.0f))
+                player.OnCollision(direction);
+
+
+
+        view.setCenter(player.GetPosition());
+
+        window.clear(sf::Color(150, 150, 150));
+        window.setView(view);
+        player.Draw(window);
+        
+        for (Platform& platform : platforms)
+        platform.Draw(window);
+      
+        window.display();
+    }
+    
+    return 0;
+}
+
